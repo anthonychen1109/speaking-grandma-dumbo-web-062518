02752794b695e4808524e0269fd14b2090490e99
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-19 16:23:06 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if !phrase.upcase
-    "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
-    "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   end
 end
 
